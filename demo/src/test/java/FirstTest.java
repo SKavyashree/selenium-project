e5db7ce531fa4fa723f21124b399863afcfde9ca
SKavyashree <kavchai@gmail.com>
2024-08-29 16:28:26 -0700

    first commit

---
+++ demo/src/test/java/FirstTest.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+class FirstTest{
+
+    // public static void main(String[] args) {
+    //     WebDriverManager.chromedriver().setup();
+    //     WebDriver driver = new ChromeDriver();
+    //   //  WebDriver driver = new FirefoxDriver();
+    //     driver.get("https://rahulshettyacademy.com");
+    //     String page_title = driver.getTitle();
+    //     System.out.println(page_title);
+    //     System.out.println(driver.getCurrentUrl());
+    //     driver.close();
+        
+    // }
+
+@Test
+    void sampleRun() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+      //  WebDriver driver = new FirefoxDriver();
+        driver.get("https://rahulshettyacademy.com");
+        String page_title = driver.getTitle();
+        System.out.println(page_title);
+        System.out.println(driver.getCurrentUrl());
+        driver.close();
+        
+    }
+}
